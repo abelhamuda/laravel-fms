580882dcf0f8c1c13a7724e2adccf785831f3d5f
abelhamuda <yozabelhamuda@gmail.com>
2023-10-11 11:41:43 +0700

    add

--- resources/lang/en/quickadmin.php
+++ resources/lang/en/quickadmin.php
@@ -246,6 +246,7 @@ return [
 	'qa_new_password' => 'New password',
 	'qa_password_confirm' => 'New password confirmation',
 	'qa_dashboard_text' => 'You are logged in!',
+	'qa_dashboard_text_2' => 'Data Visualization',
 	'qa_forgot_password' => 'Forgot your password?',
 	'qa_remember_me' => 'Remember me',
 	'qa_login' => 'Login',
